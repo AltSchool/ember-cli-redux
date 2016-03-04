01272658cbc29caad8c67df95c11391f43e275de
Matthew Constantine <designmc@gmail.com>
2016-03-04 14:08:55 -0800

    Reload the host-app upon change when the addon is npm-linked.

--- index.js
+++ index.js
@@ -7,11 +7,14 @@ var fs = require('fs');
 module.exports = {
   name: 'ember-cli-redux',
 
+  // Turn on file watching for livereload support when this addon is npm linked.
   isDevelopingAddon: function() {
-    return true; // TODO: Remove this
-  },
-
-  included: function() {
-    console.log("Including ember-cli-redux"); // TODO: Remove this
+    try {
+      fs.readlinkSync('node_modules/ember-cli-redux');
+      console.log("Ember-cli-redux npm link detected.");
+    } catch (e) {
+      return false;
+    }
+    return true;
   }
 };
